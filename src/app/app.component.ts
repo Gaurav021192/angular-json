85efea3be25cc212ea72925e54d3f98961974012
Gaurav <gauravbisht.07ms@gmail.com>
2017-10-30 22:13:30 +0530

    Angular JSON example first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+@Injectable()
+export class AppComponent implements OnInit {
   title = 'app';
+  private apiUrl = './assets/data/data.json';
+  data: any = {};
+
+  constructor(private http: Http) {
+    this.getContacts();
+    this.getData();
+  }
+
+  ngOnInit() {
+  }
+
+  getData() {
+    return this.http.get(this.apiUrl)
+      .map((res: Response) => res.json());
+  }
+
+  getContacts() {
+    this.getData().subscribe(data => {
+      console.log(data);
+      this.data = data;
+    });
+  }
 }
